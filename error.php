d020cd9b876f5728d309146e357a93194f8dac9a
Conor Hennessy <c.hennessy@outlook.com>
2018-07-02 19:11:56 +0100

    Error page complete
    commit of completed error page with supporting CSS

---
+++ error.php
@@ -0,0 +1,26 @@
+<!--PHP Boy; error page-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link rel="stylesheet" type="text/css" href="error.css">
+	<TITLE>Whoops...</TITLE>
+</head>
+
+<body>
+	<!-- Content -->
+	<div class="text">
+		<h1>
+			<B><mark>oh no...</mark></B><BR><BR>
+		</h1>
+		<h2>
+			<B>Something is broken :( </B>
+		</h2>
+	</div>
+	<!-- Footer -->
+	<div class="footer">
+			<P>
+			:(
+			</P>
+	</div>
+</body>
+</html>
