c4725940625b8f7fc98020a9d60d6b1b1561a84a
Kushcabbage <alhockly@gmail.com>
2018-07-15 15:04:46 +0100

    added update to crontab

--- UpdateFromGit.sh
+++ UpdateFromGit.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-while true
-do
-	sudo git fetch --all
-	sudo git reset --hard origin/master
-	sudo git pull
-	sudo chmod +x /var/www/html/*.sh
-	printf "updated from git"
-	sleep 5h
-done
+sudo git fetch --all
+sudo git reset --hard origin/master
+sudo git pull
+sudo chmod +x /var/www/html/*.sh
+
