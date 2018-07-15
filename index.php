541879834c35da8006f72cd0a67abc6dac8662cd
Alex <alhockly@gmail.com>
2018-07-15 15:10:33 +0100

    Clock :new: :clock3:
    Created JS clock for screen with creddit to autor here - https://www.cssscript.com/minimal-digital-clock-javascript-css/
    Moved CSS and fonts into folder
    Removed index.js file as redundant
    
    Co-Authored-By: kushcabbage <kushcabbage@users.noreply.github.com>

--- index.php
+++ index.php
@@ -1,11 +1,102 @@
-<html>
+<!DOCTYPE html>
+<html lang ="en">
 <head>
-<link rel="stylesheet" href="index.css">
+	<meta charset="utf-8">
+
+	
+	
+	<link rel="stylesheet" href="css/index.css">
+	
+	<!-- Viewport sizing for all devices -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<title> php-boy </title>
 </head>
+
 <body>
-HENLO
-<?php
-echo "<p>Hellllo in PHPPPPP pe pe</p>" ;
-?>
+<body>
+
+	<main>
+		<!-- DAY OF THE WEEK -->
+		<div class="days">
+		
+			<div class="day">
+				<p class="sunday">sunday</p>
+			</div>
+
+			<div class="day">
+				<p class="monday">monday</p>
+			</div>
+
+			<div class="day">
+				<p class="tuesday">tuesday</p>
+			</div>
+
+			<div class="day">
+				<p class="wednesday">wednesday</p>
+			</div>
+
+			<div class="day">
+				<p class="thursday">thursday</p>
+			</div>
+
+			<div class="day">
+				<p class="friday">friday</p>
+			</div>
+
+			<div class="day">
+				<p class="saturday">saturday</p>
+			</div>
+
+		</div>
+		<!-- CLOCK -->
+		<div class="clock">
+			<!-- HOUR -->
+			<div class="numbers">
+				<p class="hours"></p>
+				<p class="placeholder">88</p>
+			</div>
+
+			<div class="colon">
+				<p>:</p>
+			</div>
+
+			<!-- MINUTE -->
+			<div class="numbers">
+				<p class="minutes"></p>
+				<p class="placeholder">88</p>
+			</div>
+
+			<div class="colon">
+				<p>:</p>
+			</div>
+
+			<!-- SECOND -->
+			<div class="numbers">
+				<p class="seconds"></p>
+				<p class="placeholder">88</p>
+			</div>
+			
+			<!-- AM / PM -->
+			<div class="am-pm">
+
+				<!-- AM -->
+				<div>
+					<p class="am">am</p>
+				</div>
+
+				<!-- PM -->
+				<div>
+					<p class="pm">pm</p>
+				</div>
+			</div>
+
+		</div><!-- END CLOCK -->
+
+	</main>
+
+	
+	<script src="clock.js"> </script>
 </body>
+
 </html>
