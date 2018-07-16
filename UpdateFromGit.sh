53a2af01a94eacc73351c7c0ba5b2d864915a9fa
Conor Hennessy <c.hennessy@outlook.com>
2018-07-16 23:53:28 +0100

    No mousey :mouse:

--- UpdateFromGit.sh
+++ UpdateFromGit.sh
@@ -4,6 +4,4 @@ sudo git reset --hard origin/master
 sudo git pull origin master
 sudo chmod +x /var/www/html/*.sh
 killall chromium-browser
-sleep 1
-tmux chromium-browser --kiosk http://localhost/ & > /dev/null
-tput civis
+unclutter -idle 0
