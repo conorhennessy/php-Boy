98466ca46b0f728bce148b04b876da63a27d1847
Alex <alhockly@gmail.com>
2018-07-16 23:31:53 +0100

    at least this much works

--- UpdateFromGit.sh
+++ UpdateFromGit.sh
@@ -4,6 +4,5 @@ sudo git reset --hard origin/master
 sudo git pull origin master
 sudo chmod +x /var/www/html/*.sh
 killall chromium-browser
-sleep 1
-tmux chromium-browser --kiosk http://localhost/ & > /dev/null
+
 
