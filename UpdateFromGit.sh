50202de7a8d6b1f73b1e60f04da72275b3f6bfb8
Alex <alhockly@gmail.com>
2018-07-16 23:01:19 +0100

    updater test

--- UpdateFromGit.sh
+++ UpdateFromGit.sh
@@ -3,4 +3,9 @@ sudo git fetch --all
 sudo git reset --hard origin/master
 sudo git pull origin master
 sudo chmod +x /var/www/html/*.sh
+killall chromium-browser
+sleep 1
+tmux 
+
+chromium-browser --kiosk http://localhost/ & > /dev/null
 
