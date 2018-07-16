d8648a09229987cd2863ef01c0344ff22dc45494
Conor Hennessy <c.hennessy@outlook.com>
2018-07-16 23:40:27 +0100

    Fix Alex mucking around

--- UpdateFromGit.sh
+++ UpdateFromGit.sh
@@ -4,11 +4,6 @@ sudo git reset --hard origin/master
 sudo git pull origin master
 sudo chmod +x /var/www/html/*.sh
 killall chromium-browser
-<<<<<<< HEAD
 sleep 1
 tmux chromium-browser --kiosk http://localhost/ & > /dev/null
 tput civis
-=======
-
-
->>>>>>> a460bec63f246d6c6d71568a907ac6c051e05883
