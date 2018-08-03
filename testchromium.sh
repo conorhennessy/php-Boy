a62cf741f44447de8f879dfaa96dc7d3a0c87031
Alex <alhockly@gmail.com>
2018-08-03 02:23:40 +0100

    sleep isnt't needed

--- testchromium.sh
+++ testchromium.sh
@@ -2,7 +2,6 @@ while true
 do
 	if [[ $(pidof /usr/lib/chromium-browser/chromium-browser) ]]; then
 		echo "chromium running"
-		sleep 2m
 	else
 		echo "chromium dead"
 		chromium-browser --kiosk --incognito http://localhost/
