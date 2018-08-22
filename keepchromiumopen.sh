42fca4cf748c0030e20d7a036aab868069f5cafb
Kushcabbage <alhockly@gmail.com>
2018-08-23 00:16:10 +0100

    reboot test

--- keepchromiumopen.sh
+++ keepchromiumopen.sh
@@ -4,6 +4,8 @@ do
 		echo "chromium running"
 	else
 		echo "chromium dead"
+		sed -i 's/"exited_cleanly": false/"exited_cleanly": true/' ~/.config/chromium/Default/Preferences
+		sed -i 's/"exit_type": "Crashed"/"exit_type": "None"/' ~/.config/chromium/Default/Preferences
 		chromium-browser --kiosk --incognito http://localhost/
 	fi
 done
