9c0b83375c5a05bd7b2b5caa8fb5a0ff196f4d65
Kushcabbage <alhockly@gmail.com>
2018-08-23 12:48:59 +0100

    progress... i think

--- UpdateFromGit.sh
+++ UpdateFromGit.sh
@@ -3,9 +3,9 @@ echo "Pulling from Git"
 cd /var/www/html/
 sudo git fetch --all
 sudo git reset --hard origin/master
-sudo git pull origin master
+
 echo "updated at" > /home/pi/Desktop/Lastupdate.txt
 date >> /home/pi/Desktop/Lastupdate.txt
-
+sudo git pull origin master >> /home/pi/Desktop/Lastupdate.txt
 sudo chmod +x /var/www/html/*.sh
 killall chromium-browser
