68f80575b31641ef9eb816394a72faa4a61e82d6
Kushcabbage <alhockly@gmail.com>
2018-08-23 12:42:38 +0100

    im bad

--- UpdateFromGit.sh
+++ UpdateFromGit.sh
@@ -4,8 +4,8 @@ cd /var/www/html/
 sudo git fetch --all
 sudo git reset --hard origin/master
 sudo git pull origin master
-echo "updated at" > /home/pi/Desktop
-date >> /home/pi/Desktop
+echo "updated at" > /home/pi/Desktop/Lastupdate.txt
+date >> /home/pi/Desktop/Lastupdate.txt
 
 sudo chmod +x /var/www/html/*.sh
 killall chromium-browser
