0ced79cac787ee25cceb816e533ef4d6ae2c5ceb
Kushcabbage <alhockly@gmail.com>
2018-08-23 12:52:05 +0100

    if thais works ill be happy boy

--- UpdateFromGit.sh
+++ UpdateFromGit.sh
@@ -9,3 +9,4 @@ date >> /home/pi/Desktop/Lastupdate.txt
 sudo git pull origin master >> /home/pi/Desktop/Lastupdate.txt
 sudo chmod +x /var/www/html/*.sh
 killall chromium-browser
+sudo /var/www/html/keepchromiumopen.sh
