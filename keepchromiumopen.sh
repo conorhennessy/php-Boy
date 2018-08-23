cfbefdfe4754d1def82f98eab7f73598b7f0a9d1
Kushcabbage <alhockly@gmail.com>
2018-08-23 05:17:28 +0100

    -

--- keepchromiumopen.sh
+++ keepchromiumopen.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-
+sleep(5)
 while true
 do
 	if [[ $(pidof /usr/lib/chromium-browser/chromium-browser) ]]; then
 		echo "chromium running"
 	else
 		echo "chromium dead"
-		sed -i 's/"exited_cleanly": false/"exited_cleanly": true/' ~/.config/chromium/Default/Preferences
-		sed -i 's/"exit_type": "Crashed"/"exit_type": "None"/' ~/.config/chromium/Default/Preferences
+		
 		chromium-browser --kiosk --incognito http://localhost/
 	fi
 done
