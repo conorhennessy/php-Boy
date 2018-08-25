d9c1e182dc643de1a15b824a7e6d7d0e84b4d70e
Kushcabbage <alhockly@gmail.com>
2018-08-25 18:33:39 +0100

    weather now shows time

--- getweather.js
+++ getweather.js
@@ -4,13 +4,14 @@ xmlhttp.open("GET", "http://api.openweathermap.org/data/2.5/forecast?id=2652618&
 xmlhttp.onreadystatechange = function() {
 	if (this.readyState == 4 && this.status == 200) {
 		var Weatherobj = JSON.parse(this.responseText)				
+			
+			var days = Arrayday();
 		
-		
-			var temp0 = Math.round(Weatherobj.list[0].main.temp-273.15);
-			var temp1 = Math.round(Weatherobj.list[8].main.temp-273.15);
-			var temp2 = Math.round(Weatherobj.list[16].main.temp-273.15);
-			var temp3 = Math.round(Weatherobj.list[24].main.temp-273.15);
-			var temp4 = Math.round(Weatherobj.list[32].main.temp-273.15);
+			var temp0 = Math.round(Weatherobj.list[days[0]].main.temp-273.15);
+			var temp1 = Math.round(Weatherobj.list[days[1]].main.temp-273.15);
+			var temp2 = Math.round(Weatherobj.list[days[2]].main.temp-273.15);
+			var temp3 = Math.round(Weatherobj.list[days[3]].main.temp-273.15);
+			var temp4 = Math.round(Weatherobj.list[days[4]].main.temp-273.15);
 			
 			var weather0=Weatherobj.list[0].weather[0].main;
 			var weather1=Weatherobj.list[1].weather[0].main;
@@ -24,11 +25,11 @@ xmlhttp.onreadystatechange = function() {
 			var humidity3=Weatherobj.list[3].main.humidity;
 			var humidity4=Weatherobj.list[4].main.humidity;
 			
-			makeday(temp0,weather0,humidity0);
-			makeday(temp1,weather1,humidity1);
-			makeday(temp2,weather2,humidity2);
-			makeday(temp3,weather3,humidity3);
-			makeday(temp4,weather4,humidity4);
+			makeday(0,temp0,weather0,humidity0);
+			makeday(1,temp1,weather1,humidity1);
+			makeday(2,temp2,weather2,humidity2);
+			makeday(3,temp3,weather3,humidity3);
+			makeday(4,temp4,weather4,humidity4);
 		
 	}
 };
@@ -36,7 +37,27 @@ xmlhttp.onreadystatechange = function() {
 xmlhttp.send();
 
 
-function makeday(temp,descrip,humidity){
+function Arrayday(){
+	
+	var Dayarray=[];
+	Dayarray.push(0);
+
+	var date = new Date(),
+	hours = date.getHours();
+		var tmoz9=33-hours;
+		Dayarray.push(Math.round(tmoz9/3));		//tmoz at 9am
+		
+		Dayarray.push(Math.round((tmoz9+8)/3));	//tmoz at 5pm
+		
+		Dayarray.push(Math.round((tmoz9+24)/3)); //next day at 9am
+		
+		Dayarray.push(Math.round((tmoz9+32)/3)); ///next day at 5pm
+		
+		
+		return Dayarray;
+}
+
+function makeday(displaycount,temp,descrip,humidity){
 
 	var daybox = document.createElement("div");
 	daybox.className="weatherbox";
@@ -59,6 +80,24 @@ function makeday(temp,descrip,humidity){
 	daybox.appendChild(Humiditydiv);
 
 	
+	if(displaycount==0){
+		daybox.appendChild(document.createTextNode("Now"));
+	}
+	if(displaycount==1){
+		daybox.appendChild(document.createTextNode("9am tmoz"));
+	}
+	if(displaycount==2){
+		daybox.appendChild(document.createTextNode("5pm tmoz"));
+	}
+	if(displaycount==3){
+		daybox.appendChild(document.createTextNode("9am tmoz+1"));
+	}
+	if(displaycount==4){
+		daybox.appendChild(document.createTextNode("5pm tmoz+1"));
+	}
+	
+	
 	var element = document.getElementById("weather");
 	element.appendChild(daybox);
+	
 }
