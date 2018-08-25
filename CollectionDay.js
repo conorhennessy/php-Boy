d75e5d20b39d4c0e459cdf78219a5bef04b81509
Conor Hennessy <9257022+conorhennessy@users.noreply.github.com>
2018-08-25 22:02:59 +0100

    Complete collection info overhaul
    lil redo of the collectionDay script to use the same methord as the clock script to put the info into the HTML parts.  Plus the appropriate styling for all parts of the info box.
    Also slight move of the IP piece so that collections info will fit in.

--- CollectionDay.js
+++ CollectionDay.js
@@ -3,54 +3,49 @@ function getWeekNumber(d) {
     d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
     // Set to nearest Thursday: current date + 4 - current day number
     // Make Sunday's day number 7
-    d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay()||7));
+    d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
     // Get first day of year
-    var yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
+    var yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
     // Calculate full weeks to nearest Thursday
-    var weekNo = Math.ceil(( ( (d - yearStart) / 86400000) + 1)/7);
+    var weekNo = Math.ceil(((d - yearStart) / 86400000 + 1) / 7);
     // Return array of year and week number
     return [d.getUTCFullYear(), weekNo];
 }
 
-function PrintCollections(week){
-	element = document.getElementById("collections");
-	div = document.createElement("div");
-	
-	
-	if(week=="green"){
-		div.appendChild(document.createTextNode("green week"));
-		var desc=document.createElement("p");
-		div.appendChild(desc);
-		desc.appendChild(document.createTextNode("rubbish,glass,metal"));
-	}
-	else{
-		div.appendChild(document.createTextNode("blue week"));
-		var desc=document.createElement("p");
-		div.appendChild(desc);
-		desc.appendChild(document.createTextNode("paper,plastics"));
-	}
-	
-	element.appendChild(div);
+
+function PrintCollection(date, week) {
+    // Show what this weeks collection is...
+    weekStyle = document.getElementsByClassName("week")[0].classList;
+    if (week == "green") {
+        document.getElementsByClassName("week")[0].innerHTML = "green week";
+        document.getElementsByClassName("items")[0].innerHTML = "rubbish, glass & metal";
+        weekStyle.add("green-week");  //make it green
+    }
+    else {
+        document.getElementsByClassName("week")[0].innerHTML = "blue week";
+        document.getElementsByClassName("items")[0].innerHTML = "paper & plastics";
+        weekStyle.add("blue-week");   //make it blue   ooooh
+    }
+
+    var n = date.getDay();
+
+    if (n == 2) {
+        document.getElementsByClassName("prompt")[0].innerHTML = "It's collection day tomorrow!";
+    }
+    if (n == 3) {
+        document.getElementsByClassName("prompt")[0].innerHTML = "It's collection day today!";
+    }
 }
 
 var date = new Date();
-var n = date.getDay();
-
 var weeknum = getWeekNumber(date);
 
-
-//Our collection day is wednesday
-if(n==3){
-	
-	//odd is green, even is blue
-	if(weeknum%2==0){
-		//document.write("its a blue week");
-		//rubbish, glass, metal
-		PrintCollections("blue");
-	}
-	else{
-		//document.write("its a green week");
-		//paper, plastics
-		PrintCollections("green");
-	}
+//odd is green, even is blue
+if(weeknum % 2 == 0){
+    //Blue = rubbish, glass, metal
+    PrintCollection(date, "blue");
+}
+else{
+    //Green = paper, plastics
+    PrintCollection(date, "green");
 }
