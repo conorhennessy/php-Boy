0e326fa1f8dd1043be079e5ba63d21dc2c35bf20
Kushcabbage <alhockly@gmail.com>
2018-09-03 21:37:14 +0100

    sleep to avoid multiple chromiums

--- keepchromiumopen.sh
+++ keepchromiumopen.sh
@@ -4,10 +4,12 @@ echo "keeping chromium open"
 while true
 do
 	if [[ $(pidof /usr/lib/chromium-browser/chromium-browser) ]]; then
-		echo "chromium running"
+		echo "chromium already running"
 	else
-		echo "chromium dead"
+		echo "opening chrome"
 		
 		chromium-browser --kiosk --incognito http://localhost/index.html &
+		sleep 10
+		
 	fi
 done
