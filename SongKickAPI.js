534432b4ef2161f8c78d164768331f73f798edb5
Kushcabbage <alhockly@gmail.com>
2018-09-12 23:12:09 +0100

    added readable date for events

--- SongKickAPI.js
+++ SongKickAPI.js
@@ -26,7 +26,7 @@ function GetNextMonth(){
 					}
 					
 					printEvent(artist,venue,date);
-					console.log(artist,"@",venue,"on",date);
+					//console.log(artist,"@",venue,"on",date);
 					lastDate=date;
 				}
 				
@@ -56,25 +56,50 @@ function printEvent(artist,venue,date){
 
 function printDate(date){
 	console.log(date);
+	//var textDate= new Date(date).toString().split(" ");
+	var textDate=date.split("-")[2];	
+	textDate+=" "+getMonthName(date.split("-")[1]);
+	textDate+=" "+date.split("-")[0];
+	//console.log(textDate);
 	var targetDiv = document.getElementById("LiveMusic");
 	var DateHeader = document.createElement("div");
 	DateHeader.className="MusicDate";
-	DateHeader.innerHTML=date;
+	DateHeader.innerHTML=textDate;
 	targetDiv.appendChild(DateHeader);
 	
 }
 
+function getMonthName(monthNum){
+    var month = new Array();
+    month[1] = "January";
+    month[2] = "February";
+    month[3] = "March";
+    month[4] = "April";
+    month[5] = "May";
+    month[6] = "June";
+    month[7] = "July";
+    month[8] = "August";
+    month[9] = "September";
+    month[10] = "October";
+    month[11] = "November";
+    month[12] = "December";
+   
+    return month[parseInt(monthNum)];
+}
+
 function StartOfNextMonth(){
 	var date = new Date();
-	var nextmonth=date.getMonth()+2;
+	var nextmonth=parseInt(date.getMonth())+2;	//plus 2 because js counts months from 0 (and songkick does not)
+	console.log(nextmonth);
 	nextmonth = ("0" + nextmonth).slice(-2);  //make sure month is two digits
 	var min_date = date.getFullYear()+"-"+nextmonth+"-"+"01";
+	console.log(min_date);
 	return min_date;
 }
 
 function EndOfNextMonth(){
   var date = new Date();
-  var nextmonth=date.getMonth()+2;
+  var nextmonth=date.getMonth()+2	;
   nextmonthpadded = ("0" + nextmonth).slice(-2);  //make sure month is two digits
   var max_date = date.getFullYear()+"-"+nextmonthpadded+"-"+daysInMonth(nextmonth,date.getFullYear());
   return max_date;
