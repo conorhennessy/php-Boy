fceb4df60e4c4c3d286b2441d49bfa7ce0683bed
Alex <alhockly@gmail.com>
2018-09-21 13:55:02 +0100

    f

--- getweek.py
+++ getweek.py
@@ -1,7 +1,7 @@
 
 import os
 
-os.system("wget -o collectionpage.html https://www.colchester.gov.uk/check-my-collection-day/?query=aae96a3d-6027-e711-80fa-5065f38b56d1&name=18%20Chaney%20Road.html")
+os.system("wget -O collectionpage.html https://www.colchester.gov.uk/check-my-collection-day/?query=aae96a3d-6027-e711-80fa-5065f38b56d1&name=18%20Chaney%20Road.html")
 
 with open('collectionpage.html', 'r') as myfile:
     pagetext=myfile.read()
