43fea9b6fce0d47e6f3c0c659a317ae3d4b7f425
Alex <alhockly@gmail.com>
2018-09-22 12:43:02 +0100

    merge

--- collectionquery.js
+++
@@ -1,9 +0,0 @@
-var xhr = new XMLHttpRequest();
-xhr.onload = function() {
-  console.log(this.responseXML.title);
-}
-xhr.open("GET", "https://www.colchester.gov.uk/check-my-collection-day/?query=aae96a3d-6027-e711-80fa-5065f38b56d1&name=18%20Chaney%20Road#ValidationSummaryEntityFormView.html");
-xhr.responseType = "document";
-xhr.send();
-
-
