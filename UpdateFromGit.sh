84ceff94af3e5aa139e4496b79b2ccfc73bea203
Alex <alhockly@gmail.com>
2018-11-03 14:15:42 +0000

    put js in its own dir

--- UpdateFromGit.sh
+++ UpdateFromGit.sh
@@ -8,5 +8,6 @@ echo "updated at" > /home/pi/Desktop/Lastupdate.txt
 date >> /home/pi/Desktop/Lastupdate.txt
 sudo git pull origin master >> /home/pi/Desktop/Lastupdate.txt
 sudo chmod +x /var/www/html/*.sh
+sudo chmod 777 /var/ww/html/*.txt
 killall chromium-browser
 
